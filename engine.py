78d57fae9dba997dbde34fa0d90dddbcf9f21965
js-machinecode <jsoto.machinecode@gmail.com>
2026-06-05 15:21:56 -0400

    made the game board

--- engine.py
+++ engine.py
@@ -5,3 +5,15 @@ class ChessGame:
     def __init__(self):
         self.board = self.create_board()
         self.turn = "white"
+
+    def create_board(self):
+        return [
+            ["r", "n", "b", "q", "k", "b", "n", "r"],
+            ['p' for _ in range(8)],
+            ['.' for _ in range(8)],
+            ['.' for _ in range(8)],
+            ['.' for _ in range(8)],
+            ['.' for _ in range(8)],
+            ['P' for _ in range(8)],
+            ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+        ]
