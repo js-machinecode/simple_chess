62940936427956e1b9c6ae1fa11317c3491403f7
js-machinecode <jsoto.machinecode@gmail.com>
2026-06-05 15:40:43 -0400

    made test of white pawn forward move one

--- test_engine.py
+++ test_engine.py
@@ -9,3 +9,8 @@ class TestChessGame(unittest.TestCase):
 
     def test_initial_turn_is_white(self):
         self.assertEqual(self.game.turn, "white")
+
+    def test_white_pawn_move_forward_one(self):
+        success, message = self.game.move_piece('e2', 'e3')
+        self.assertTrue(success)
+        self.assertEqual(self.game.board[5][4], 'P')
